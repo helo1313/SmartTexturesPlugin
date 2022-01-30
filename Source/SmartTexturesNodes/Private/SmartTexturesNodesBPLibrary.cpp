4d7bbad84c3b913a4cb8dc185be97364941d6cd7
Helo <62700189+helo1313@users.noreply.github.com>
2022-01-30 22:55:51 +0100

    [code] Initial Plugin setup

---
+++ Source/SmartTexturesNodes/Private/SmartTexturesNodesBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SmartTexturesNodesBPLibrary.h"
+#include "SmartTexturesNodes.h"
+
+USmartTexturesNodesBPLibrary::USmartTexturesNodesBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USmartTexturesNodesBPLibrary::SmartTexturesNodesSampleFunction(float Param)
+{
+	return -1;
+}
+
